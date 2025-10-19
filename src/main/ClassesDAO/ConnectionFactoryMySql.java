83eb9a4f30fe6aa9a2f53c1cd9ef51b3cdb36a32
k4io404 <116021310+k4io404@users.noreply.github.com>
2025-10-18 23:04:19 -0300

    Update ConnectionFactoryMySql.java

--- src/main/ClassesDAO/ConnectionFactoryMySql.java
+++ src/main/ClassesDAO/ConnectionFactoryMySql.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class ConnectionFactory {
+public class ConnectionFactoryMySql {
 
     private static final String URL = "jdbc:mysql://localhost:3306/nomeBanco";
     private static final String USER = "root";
